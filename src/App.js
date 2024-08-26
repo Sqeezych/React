eb2935d4512a58b4ae8039562e3f3c9b3316a1f4
sqeezy <sqeezy98@gmail.com>
2024-08-26 15:52:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const currentYear = new Date().getFullYear()
+const currentMonth = new Date().getMonth() + 1
+const currentDay = new Date().getDate()
 
-export default App;
+// export - императивный. Потому что это реализация нужного кода
+export const App = () => {
+	// return html код - декларативный. Потому что мы указываем, что хотим видеть на выходе
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<h2>{currentDay}/{currentMonth}/{currentYear}</h2>
+			</header>
+		</div>
+	);
+};
